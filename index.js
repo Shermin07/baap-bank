ec1747f41efc7fd0e04c5fc3eb9332c27f922b4a
Shermin Sultana <rima64549@gmail.com>
2023-08-15 05:53:16 +0600

    bank

---
+++ index.js
@@ -0,0 +1,19 @@
+document.getElementById("btn-submit").addEventListener('click', function(){
+   const emailField = document.getElementById("user-email");
+   const email = emailField.value;
+   
+   const passwordField = document.getElementById("user-password");
+   const password = passwordField.value;
+
+   if(email === "shermin@gmail.com" && password === "1234"){
+    window.location.href = "bank.html"
+   }
+   else{
+    alert("please give correct password")
+   }
+
+   
+})
+
+// part 2::
+
