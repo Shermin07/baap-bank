285d97ea0f45a776488af36f390e6d77eae2661a
Shermin Sultana <rima64549@gmail.com>
2023-08-15 14:31:56 +0600

    add

--- deposite.js
+++ deposite.js
@@ -3,5 +3,23 @@ document.getElementById("btn-deposite").addEventListener('click', function(){
     const depositeField = document.getElementById("deposite-field");
     const newDepositeAmountString = depositeField.value ;
     const newDepositeAmount = parseFloat(newDepositeAmountString);
-    console.log(newDepositeAmount);
+    //console.log(newDepositeAmount);
+
+
+    const depositeTotalElement = document.getElementById("deposite-total");
+    const previousDepositeTotalSting = depositeTotalElement.innerText ;
+    const previousDepositeTotal = parseFloat(previousDepositeTotalSting);
+
+    //console.log(previousDepositeTotal);
+
+    const currentDepositeTotal = previousDepositeTotal + newDepositeAmount ;
+    depositeTotalElement.innerText = currentDepositeTotal;
+
+
+
+
+
+
+
+    depositeField.value = " ";
  })
